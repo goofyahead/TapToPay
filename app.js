36588c6eea89e9c80d19958dbf136fd65aef43c4
Alex Vidal <goofyahead@gmail.com>
2013-09-28 13:21:57 -0700

    port to 7

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var gpio = require("gpio");
 var colors = require("colors");
 
-var gpio4 = gpio.export(4, {
+var gpio4 = gpio.export(7, {
    direction: "in",
    ready: function() {
    	console.log('BUTTON WAS PUSHED!'.green);
