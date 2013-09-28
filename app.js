021c91d39324fdde4ca41edc6bf2fd4dbac02409
Alex Vidal <goofyahead@gmail.com>
2013-09-28 13:27:47 -0700

    change function

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var colors = require("colors");
 var gpio4 = gpio.export(7, {
    direction: "in",
    ready: function() {
-   	console.log('BUTTON WAS PUSHED!'.green);
+   	console.log('*******************');
+   	console.log('*  Ready to work  *');
+   	console.log('*******************');
+   	
+   	gpio4.on("change", function(val){
+   		console.log('BUTTON WAS PUSHED!'.green);
+   	});
    }
 });
